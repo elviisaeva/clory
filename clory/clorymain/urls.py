125fe72c1831bd83fa641f4190f52900bf3564b8
elviisaeva <el.isaeva2000@gmail.com>
2019-12-21 01:30:00 +0300

    Women category

--- clory/clorymain/urls.py
+++ clory/clorymain/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name = 'home'),
+    path('', views.HomeView.as_view(), name = 'all-stores'),
+    path('zara/', views.zara, name = 'zara'),
+    path('zara/zara_women/', views.zara_women, name = 'zara_women'),
+    path('zara/zara_women/zara_all/', views.zara_all, name = 'zara_all'),
+    path('zara/zara_women/zara_dress/', views.zara_dress, name = 'zara_dress'),
+    path('zara/zara_women/zara_tshirt/', views.zara_tshirt, name = 'zara_tshirt'),
+    path('zara/zara_women/zara_trouser/', views.zara_trouser, name = 'zara_trouser'),
+
 ]
--- clory/clorymain/views.py
+++ clory/clorymain/views.py
@@ -5,6 +5,23 @@ from clory_data.models import Product
 def home(request):
     return render(request, 'home.html')
 
+def zara(request):
+    return render(request, 'zara.html')
+
+def zara_women(request):
+    return render(request, 'zara_women.html')
+
+def zara_all(request):
+    return render(request, 'zara_all.html') 
+
+def zara_dress(request):
+    return render(request, 'zara_dress.html')
+
+def zara_tshirt(request):
+    return render(request, 'zara_tshirt.html')
+
+def zara_trouser(request):
+    return render(request, 'zara_trouser.html')
 
 class HomeView(ListView):
     model = Product
